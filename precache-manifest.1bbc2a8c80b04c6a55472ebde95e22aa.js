66971d62cee4a614215fa7053be027ae9601851b
SamVimes0 <k.aleksandr88@gmail.com>
2019-02-26 21:17:26 +0200

    Updates

---
+++ precache-manifest.1bbc2a8c80b04c6a55472ebde95e22aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c60fe4cd649ed4553ef",
+    "url": "/todo-react/static/css/main.9e7f0e83.chunk.css"
+  },
+  {
+    "revision": "3c60fe4cd649ed4553ef",
+    "url": "/todo-react/static/js/main.3c60fe4c.chunk.js"
+  },
+  {
+    "revision": "3a890a3f15edfdf19ca0",
+    "url": "/todo-react/static/js/runtime~main.3a890a3f.js"
+  },
+  {
+    "revision": "0db3428c39ac987d3d2f",
+    "url": "/todo-react/static/js/2.0db3428c.chunk.js"
+  },
+  {
+    "revision": "b9ef0c8184dbc0d44077a286edca008f",
+    "url": "/todo-react/index.html"
+  }
+];
